94300213ded055bee66a35e32e727b222f06bf40
Chaz Beck <chbeck@microsoft.com>
2023-01-09 14:20:47 -0800

    Added DiffEngineTray

--- src/init.ps1
+++ src/init.ps1
@@ -7,6 +7,7 @@ if ($env:CentralPackageManagementMigratorInitLoaded) {
     return
 }
 
+# Load Scripts directory
 $scriptsDir = (Get-Item "$PSScriptRoot\..\scripts").FullName
 
 if($env:Path -split ';' -inotcontains $scriptsDir) {
@@ -22,6 +23,23 @@ foreach ($script in $scripts) {
 }
 
 Write-Host "Any script (*.ps1) in $scriptsDir can be called with just name of script" -ForegroundColor Gray
+
+# Load Dotnet tool
+$dotnetToolConfigPath = (Get-Item "$PSScriptRoot\.config\dotnet-tools.json").FullName
+$dotnetToolConfig = Get-Content -Path $dotnetToolConfigPath | ConvertFrom-Json -Depth 100
+$dotnetToolNames = $dotnetToolConfig.tools.psobject.Members | Where-Object MemberType -eq 'NoteProperty'
+
+Write-Host "Restoring dotnot tools"
+dotnet tool restore
+
+foreach ($toolName in $dotnetToolNames) {
+    $toolCommands = Invoke-Expression "`$dotnetToolConfig.tools.$($toolName.Name).commands"
+    foreach ($commandName in $toolCommands) {
+        Invoke-Expression -Command "function global::DOTNETTOOL_$commandName { param([Parameter(ValueFromRemainingArguments)] `$args) & dotnet tool run $commandName $args }" 
+        Set-Alias -Name $commandName -Value "global::DOTNETTOOL_$commandName" -Scope Global -Option AllScope -ErrorAction SilentlyContinue
+    }
+}
+
 
 # Load VS CMD
 LoadVSDevConsole -VSPreview:($VSPreview.IsPresent)
